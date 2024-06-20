cc4b53f0958772cd900ef87b8a49ea549b6e2eb8
ABDOU SOULEYMANE <abdou.souleymane@ninetec.ne>
2024-06-20 12:35:43 +0100

    Reset password part 1

--- accounts/urls.py
+++ accounts/urls.py
@@ -21,18 +21,22 @@ urlpatterns = [
     path('delete_order/<str:pk>/', deleteOrder.deleteOrder, name="delete_order"),
     
     path('reset_password/', 
-         auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), 
+         auth_views.PasswordResetView.as_view
+         (template_name="accounts/password_reset.html"), 
          name="reset_password"),
     
     path('reset_password_sent/', 
-         auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), 
+         auth_views.PasswordResetDoneView.as_view
+         (template_name="accounts/password_reset_sent.html"), 
          name="password_reset_done"),
     
     path('reset/<uidb64>/<token>/', 
-         auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), 
+         auth_views.PasswordResetConfirmView.as_view
+         (template_name="accounts/password_reset_form.html"), 
          name="password_reset_confirm"),
     
     path('reset_password_complete/', 
-         auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), 
+         auth_views.PasswordResetCompleteView.as_view
+         (template_name="accounts/password_reset_done.html"), 
          name="password_reset_complete"),
 ]
